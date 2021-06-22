1de61f4abe523b41de6fd02eedcdad199351bb70
igah franklin <igahfranklyn@gmail.com>
2021-06-22 12:07:55 +0100

    djurl

--- parachCRM/settings.py
+++ parachCRM/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-import dj_datbase_url
+import dj-database-url
 from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
